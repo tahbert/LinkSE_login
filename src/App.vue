500013fc16c917ffc638a67aad8e392b267ceb1f
tahbert <tahbertnguyen@gmail.com>
2023-03-01 16:55:12 +0700

    responsive + fixed height + error

--- src/App.vue
+++ src/App.vue
@@ -27,10 +27,19 @@ export default {
   methods: {
     submitForm() {
       this.v$.$validate();
+      // if (this.v$.$error) {
+      //   for (let key in Object.keys(this.v$)) {
+      //     const input = Object.keys(this.v$)[key];
+      //     if (input.includes("$")) return false;
+      //     if (this.state.v$[input].$error) {
+      //       this.state.$refs[input].focus();
+      //       break;
+      //     }
+      //   }
+      // }
       if (!this.v$.$error) {
         setTimeout(() => {
           this.state.alwaysFail = "Invalid User Credentials";
-          console.log(this.state.alwaysFail);
         }, 2000);
       }
     },
@@ -51,29 +60,43 @@ export default {
       <img class="primary-logo" src="/logo.svg" alt="linkSE" />
 
       <form class="login-form" @submit.prevent="submitForm">
-        <label class="url" for="url">
+        <label class="box-input url" for="url">
           Base URL
-          <input type="text" id="url" autofocus v-model="state.url" />
+          <input type="text" id="url" autofocus v-model="state.url" ref="url" />
           <div class="error-input" v-if="v$.url.$error">
             {{ v$.url.$errors[0].$message }}
           </div>
           <span class="url-fixed">.linkse.dev</span>
         </label>
-        <label for="username">
+        <label class="box-input" for="username">
           Username
-          <input type="text" id="username" v-model="state.username" />
+          <input
+            type="text"
+            id="username"
+            v-model="state.username"
+            ref="username"
+          />
           <div class="error-input" v-if="v$.username.$error">
             {{ v$.username.$errors[0].$message }}
           </div>
         </label>
-        <label for="password">
+        <label class="box-input" for="password">
           Password
-          <input type="password" id="password" v-model="state.password" />
+          <input
+            type="password"
+            id="password"
+            v-model="state.password"
+            ref="password"
+          />
           <div class="error-input" v-if="v$.password.$error">
             {{ v$.password.$errors[0].$message }}
           </div>
         </label>
+
         <div class="login-form_bottom">
+          <div class="error" v-if="state.alwaysFail !== ''">
+            {{ state.alwaysFail }}
+          </div>
           <label class="container">
             Remember me
             <input type="checkbox" checked="checked" />
@@ -83,10 +106,6 @@ export default {
         </div>
         <button class="btn-submit">Login</button>
       </form>
-
-      <div class="error" v-if="state.alwaysFail !== ''">
-        {{ state.alwaysFail }}
-      </div>
     </div>
   </div>
 </template>
